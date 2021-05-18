00705bd5960e0b755bde3e3c31d98c842b8b9f46
Amir <ganizhev.amir@mail.ru>
2021-05-18 23:21:01 +0300

    commit

---
+++ js/todo.js
@@ -0,0 +1,26 @@
+let listInput = document.querySelector(".listInput");
+
+let listAdd = document.querySelector(".listAdd");
+
+let todoList = document.querySelector("#todoList");
+
+let total = document.querySelector(".total");
+
+listAdd.onclick = () => {
+	todoList.innerHTML += `<div class="todo-list">
+						   		<h2>Номер 1</h2>
+						   		<textarea>${listInput.value}</textarea>
+						   		<button class="delete-button">Удалить ✗</button>
+						   		<button class="edit-button">Сохранить ジ</button>
+						   		<button class="marker-button">Отметить ✓</button>
+						   </div>`;
+	listInput.value = "";
+
+	let deleteButton = document.querySelectorAll(".delete-button");
+
+	for(let i = 0, i <= )
+
+	deleteButton.onclick = () => {
+		console.log('log');
+	}
+}
