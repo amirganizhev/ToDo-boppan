26d6994ebd40b840a26c71afc7a5b8d33f631e72
Amir <ganizhev.amir@mail.ru>
2021-06-09 22:18:33 +0300

    commit

--- js/todo.js
+++ js/todo.js
@@ -1,26 +1,51 @@
-let listInput = document.querySelector(".listInput");
-
-let listAdd = document.querySelector(".listAdd");
-
-let todoList = document.querySelector("#todoList");
-
-let total = document.querySelector(".total");
+/*Кнопка добавления задания*/
+const listAdd = document.querySelector(".listAdd");
 
 listAdd.onclick = () => {
-	todoList.innerHTML += `<div class="todo-list">
+
+	/*Форма добавления задания*/
+	const taskInput = document.querySelector(".task-input");
+	/*Блок списка дел*/
+	const todoList = document.querySelector("#todoList");
+
+	if(taskInput.value == "") {
+		return false;
+	}
+
+	todoList.innerHTML += `<div class="todo-task">
 						   		<h2>Номер 1</h2>
-						   		<textarea>${listInput.value}</textarea>
+						  		<p>${taskInput.value}</p>
 						   		<button class="delete-button">Удалить ✗</button>
-						   		<button class="edit-button">Сохранить ジ</button>
 						   		<button class="marker-button">Отметить ✓</button>
-						   </div>`;
-	listInput.value = "";
+						   	</div>`;
 
-	/*let deleteButton = document.querySelectorAll(".delete-button");
+	taskInput.value = "";
 
-	for(let i = 0, i <= )
+	/*Карточка задания*/
+	const todoTask = document.querySelectorAll(".todo-task");
+	/*Удаление задания*/
+	const deleteButton = document.querySelectorAll(".delete-button");
+	/*Отметка задания*/
+	const markerButton = document.querySelectorAll(".marker-button");
+	
+	setTimeout(function() {
+		for(let i = 0; i <= deleteButton.length; i++) {
+		deleteButton[i].onclick = () => {
+			for(let j = 0; j <= todoTask.length; j++) {
+				todoTask[i].style.display = 'none';
+			}
+		}
+	}
+	}, 0)
+	
+	setTimeout(function() {
+		for(let k = 0; k <= markerButton.length; k++) {
+		markerButton[k].onclick = () => {
+			for(let l = 0; l <= todoTask.length; l++) {
+				todoTask[k].style.backgroundColor = 'red';
+			}
+		}
+	}
+	}, 0)
 
-	deleteButton.onclick = () => {
-		console.log('log');
-	}*/
 }
